74874943f1901b23967cef3bd13e2e0196654421
rrobaina <rrobaina@antel.net.uy>
2020-07-01 14:04:50 -0300

    Updates

---
+++ precache-manifest.4f4ea9bbd9ec71d7087c40188ea1529e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2069c35d5e591bfac6c010743f69dd3",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "80075d885236e2b9ab64",
+    "url": "/monsters/static/css/main.56c29fd4.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80075d885236e2b9ab64",
+    "url": "/monsters/static/js/main.5471ccac.chunk.js"
+  },
+  {
+    "revision": "92788b254d42c8cdca2a",
+    "url": "/monsters/static/js/runtime-main.a7930c39.js"
+  }
+]);
